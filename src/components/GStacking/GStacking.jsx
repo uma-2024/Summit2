c88694a6e98a201570b99927de91fdc6879e2910
uma-2024 <uma@web5solution.com>
2024-12-13 15:58:06 +0530

    commit8

--- src/components/GStacking/GStacking.jsx
+++ src/components/GStacking/GStacking.jsx
@@ -12,7 +12,7 @@ const GStacking = () => {
       <div className="g-left">
         <h1>
           How
-          <span>Unite-to-earn</span>WORKS
+          <span>Unite-to-earn</span>Works
         </h1>
         <p>
           The sole project with a thriving live ecosystem. It has maintained a
